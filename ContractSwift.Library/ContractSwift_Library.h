d704a91728af93964d558c09654e008dd5e23ef6
daisuke310vvv <daisuke.sato1991@gmail.com>
2018-04-04 00:57:54 +0900

    Initial Commit

---
+++ ContractSwift.Library/ContractSwift_Library.h
@@ -0,0 +1,19 @@
+//
+//  ContractSwift_Library.h
+//  ContractSwift.Library
+//
+//  Created by Daisuke Sato on 2018/04/04.
+//  Copyright © 2018 Daisuke Sato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ContractSwift_Library.
+FOUNDATION_EXPORT double ContractSwift_LibraryVersionNumber;
+
+//! Project version string for ContractSwift_Library.
+FOUNDATION_EXPORT const unsigned char ContractSwift_LibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ContractSwift_Library/PublicHeader.h>
+
+
